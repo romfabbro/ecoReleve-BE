1de19dae4577bcce330c5217baa33c590048e013
romfabbro <romain_fabbro@natural-solutions.eu>
2018-03-21 11:36:30 +0100

    fix init resource before getitem

--- Back/ecoreleve_be_server/Views/protocols.py
+++ Back/ecoreleve_be_server/Views/protocols.py
@@ -70,6 +70,7 @@ class ObservationsView(DynamicObjectCollectionView):
 
     Collection = None
     item = ObservationView
+    children = [('{int}', ObservationView)]
     moduleFormName = 'ObservationForm'
     moduleGridName = 'ObservationFilter'
 
@@ -298,4 +299,4 @@ class ObservationsView(DynamicObjectCollectionView):
         return res
 
 
-RootCore.listChildren.append(('protocols', ObservationsView))
+RootCore.children.append(('protocols', ObservationsView))
